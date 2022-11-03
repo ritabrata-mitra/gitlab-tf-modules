cb9d810612e040ce64b8571dbfa0861462b81a3a
ritabrata-mitra <105199843+ritabrata-mitra@users.noreply.github.com>
2022-11-03 12:13:53 +0530

    Create ec2.tf
    This module is to create ec2 module using terraform

---
+++ ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "new_instance" {
+  region = var.region
+  vpc = var.vpc_id
+  subnet = var.subnet_id
+  publicip = false
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name = var.key_name
+  for_each           = toset(var.sg_name)
+  security_groupname = ${each.value}
+  tags = {
+    Name = var.name
+  }
+}
+
+#Define inputs for using the module #
+
+variable "region" {}
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "ami_id" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "sg_name" {}
+variable "name" {}
+
+#Define outpus of the module#
+
+output "private_ip"{
+  value = aws_instance.new_instance.private_ip
+}
