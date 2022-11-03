b82ae48f753622a0b859b5d87baf34cf93fd7f8f
ritabrata-mitra <105199843+ritabrata-mitra@users.noreply.github.com>
2022-11-03 12:55:54 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "server1" {
+resource "aws_instance" "new_instance" {
   ami           = var.ami
   instance_type = var.instance_type
   tags = {
@@ -12,3 +12,11 @@ variable "name" {}
 variable "instance_type" {}
 variable "key_name" {}
 variable "ami" {}
+
+#Define outpus of the module#
+output "private_ip"{
+  value = aws_instance.new_instance.private_ip
+}
+output "public_ip" {
+  value = aws_instance.new_instance.public_ip
+}
